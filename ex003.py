42a0d19f0116943dbf094b75cdb9c62604fb213c
Georgia Cordeiro <ra99050@uem.br>
2021-08-13 12:07:40 -0300

    Tipos primitivos

---
+++ ex003.py
@@ -0,0 +1,13 @@
+n1 = int(input('digite um numero: '))
+n2 = int(input('digite outro numero: '))
+s = n1 + n2
+print('A soma dos numeros {} e {} é {}!: '.format(n1,n2, s) )
+
+# TIPOS PRIMITIVOS
+# dentro do input, mesmo que seja um número, ele é considerado uma string
+# deve-se converter o input para inteiro para realizar a soma
+# outros tipos:
+# -int: positivos, negativos ou nulos
+# -float: reais
+# -bool: true ou false
+# -string: caracteres
