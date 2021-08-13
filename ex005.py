1b55c268b90b21bc9013b137afffb3fb7532494a
Georgia Sonoda <georgia.sonoda@xcelis.com.br>
2021-08-13 19:23:35 -0300

    Alterações

--- ex005.py
+++ ex005.py
@@ -1,6 +1,6 @@
-var=float(input('Digite um número: '))
-suc=var+1
-ant=var-1
+var = float(input('Digite um número: '))
+suc = var+1
+ant = var-1
 print('O número digitado é: ', var)
 print('O seu sucessor é: ', suc)
 print('o seu antecessor é: ', ant)
