398528ac3a31a9b9e487eb920b879ff3b38dc4ef
Georgia Cordeiro <georgia.cordeiro97@gmail.com>
2021-08-14 22:14:18 -0300

    criação da pasta do Curso em Vídeo

---
+++ Exercícios python.org/15.py
@@ -0,0 +1,11 @@
+
+a = float(input('Quanto você ganha por hora em reais? '))
+b = float(input('Quantas horas você trabalha por dia? '))
+
+salario = (a*b*22)
+
+c = bool(input('Seu salário é de aproximadamente {} por mês? [0-Sim ou 1-Não]'.format(salario)))
+
+if c = False:
+          input(a)
+          input(b)
